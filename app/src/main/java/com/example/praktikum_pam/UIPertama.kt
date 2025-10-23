75f823edbf1f9d28d710b506014882b238bba92c
nabilasaf <nabilasafitri861@gmail.com>
2025-10-23 08:59:34 +0700

    Menambahkan ROW kedalam Column

--- app/src/main/java/com/example/praktikum_pam/UIPertama.kt
+++ app/src/main/java/com/example/praktikum_pam/UIPertama.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
@@ -45,6 +46,7 @@ fun ActivitasPertama(modifier: Modifier){
                 containerColor = Color.DarkGray
             )){
 
+            }
         }
 
     }
