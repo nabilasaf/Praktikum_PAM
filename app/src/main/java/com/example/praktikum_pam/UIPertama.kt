6894a9e93eddd16215852c7e21c6e3add29bc1d0
nabilasaf <nabilasafitri861@gmail.com>
2025-10-23 15:26:24 +0700

    Menambahkan Layout di Card

--- app/src/main/java/com/example/praktikum_pam/UIPertama.kt
+++ app/src/main/java/com/example/praktikum_pam/UIPertama.kt
@@ -69,7 +69,12 @@ fun ActivitasPertama(modifier: Modifier){
                     )
                 }
             }
-            Card {  }
+            Card (modifier = Modifier
+                .fillMaxWidth(fraction = 1f)
+                .padding(all = 15.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = Color.DarkGray
+                )){  }
         }
         Box(
             modifier = Modifier
